0b79a33bb9af9ce7bd72c1ac35e4d98982183ff8
Mike Zevitas <mzevitas@gmail.com>
2015-09-30 09:48:48 -0400

    fixed a bunch of stuff on footer

--- homepage.js
+++ homepage.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+        anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],
          menu: '#myMenu',
          menu: true,
-         sectionsColor: ['', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+         sectionsColor: ['', '', '', '', '#bc357b', '#000'],
          navigation: true,
+    css3: true,
+    scrollingSpeed: 1000,
+    scrollOverflow: true,
 
-         
+
          onLeave: function(anchor, index){
           if(index > 3){
-               $('#fixedForm').fadeOut(20);
+               $('#fixedForm').hide("slide", { direction: "up" }, 100);
           }
           else {
-          	$('#fixedForm').fadeIn(1000);
+            $('#fixedForm').show("slide", { direction: "down" }, 800);
           }
       }
 
+      
     });
 });
 
